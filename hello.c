b57f6105b1791cc2664901cd93d12532288926bc
Mohammed <zkarkoosh2@huskers.unl.edu>
2023-08-20 18:36:38 -0600

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+*/
+
+int main(int argc, char **argv){
+
+    printf("Hello, I'm Mohammed Karkoosh!\n");
+    printf("Major is Computer Science!\n");
+
+   return 0;
+}
