3a40dfde5913286138a29bf520c2721db1f2b1b5
MohammedUNL <142268647+MohammedUNL@users.noreply.github.com>
2023-08-23 12:36:17 -0600

    Fixed Liberary Bug

--- hello.c
+++ hello.c
@@ -1,11 +1,12 @@
 /**
  * Basic Hello World Program
 */
-
+#include <stdlib.h>
+#include <stdio.h>
 int main(int argc, char **argv){
 
     printf("Hello, I'm Mohammed Karkoosh!\n");
     printf("Major is Computer Science!\n");
 
-   return 0;
+    return 0;
 }
