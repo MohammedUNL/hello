f7bece18ac07aa7ecfec1ea3381939fa31ea195d
MohammedUNL <142268647+MohammedUNL@users.noreply.github.com>
2023-08-23 12:45:56 -0600

    added my minor

--- hello.c
+++ hello.c
@@ -7,6 +7,7 @@ int main(int argc, char **argv){
 
     printf("Hello, I'm Mohammed Karkoosh!\n");
     printf("Major is Computer Science!\n");
-
+    printf("Minore is Japanese\n");
+    
     return 0;
 }
