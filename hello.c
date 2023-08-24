413cdf1ca507404e9986ae7f06ee701853deb385
MohammedUNL <142268647+MohammedUNL@users.noreply.github.com>
2023-08-24 09:14:40 -0600

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
-/**
+/*         mohammedkakkroosh@gmail.com
+ * Date: 2023/16/08
+ *
  * Basic Hello World Program
-*/
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 int main(int argc, char **argv){
@@ -8,6 +11,6 @@ int main(int argc, char **argv){
     printf("Hello, I'm Mohammed Karkoosh!\n");
     printf("Major is Computer Science!\n");
     printf("Minore is Japanese\n");
-    
+
     return 0;
 }
