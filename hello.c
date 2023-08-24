8e63c3f9dbfaa76b780312f18b3e85a993fc4739
Mustafa Waly <mwaly2@huskers.unl.edu>
2023-08-24 12:50:18 -0500

    added Mustafa

--- hello.c
+++ hello.c
@@ -11,6 +11,8 @@ int main(int argc, char **argv){
     printf("Hello, I'm Mohammed Karkoosh!\n");
     printf("Major is Computer Science!\n");
     printf("Minore is Japanese\n");
+    printf("Hello, I'm Mustafa Waly!\n");
+    printf("My Major is Computer Science!\n");
 
     return 0;
 }
